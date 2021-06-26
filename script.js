f3a91e8cb4963ba593d601492b606a46c015b4a1
Aabhas <arinabansal@gmail.com>
2021-06-26 10:58:18 +0530

    Added normal coords for copying

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function copy(){
     //getting the command and transforming it
     let content = document.getElementById("INPUT").value;
     document.getElementById("command").value = "/execute in minecraft:the_nether run tp @s "+((Math.floor(content.split(" ")[6]))/8)+" "+((Math.floor(content.split(" ")[7])))+" "+((Math.floor(content.split(" ")[8]))/8);
+    document.getElementById("coords").value = ((Math.floor(content.split(" ")[6]))/8)+" "+((Math.floor(content.split(" ")[7])))+" "+((Math.floor(content.split(" ")[8]))/8);
     document.getElementById("copy").click()
 }
 
